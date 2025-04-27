8e45901ac69753c35d02c2fa24e745efc0a7a083
Temwani Msiska <.com temwani.msiska@gmail.com>
2025-04-27 11:22:48 +0200

    Updated middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,18 @@
+// config/middlewares.ts
+
 export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://temwanimsiska.dev'], 
+      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
